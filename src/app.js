e82aee4c9e21ef609a9b3e8da8f51f5ef770623a
Dev0711 <111134895+Dev0711@users.noreply.github.com>
2024-12-24 14:29:15 +0530

    simple authentication

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.get("/admin/getAlldata", (req, res) => {
   res.send("admin data");
 });
 app.get("/user", userAuth, (req, res) => {
+  // we can write userAuth, pass like this aslo
   res.send("this is user auth");
 });
 app.listen(7777, () => {

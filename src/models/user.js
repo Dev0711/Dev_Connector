361c5706cce23ace564329f0148322d596916656
Dev0711 <111134895+Dev0711@users.noreply.github.com>
2024-12-25 17:56:48 +0530

    User Schema Updated

--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,53 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      require: true,
+      minlength: 2,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      require: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      require: true,
+
+      validate(value) {
+        if (!["male", "female", "other"].includes(value)) {
+          throw new Error("invalid gender");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
+    about: {
+      type: String,
+      default: "Defult about",
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("USer", userSchema);
